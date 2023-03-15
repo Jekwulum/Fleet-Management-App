ad3181ea427acf730b0c96e7927e90dc33d3556f
Charles <charlesnwoye2@gmail.com>
2023-03-15 14:42:35 +0100

    modified driverTableConfig

--- src/utils/dataTableConfig.js
+++ src/utils/dataTableConfig.js
@@ -1,6 +1,18 @@
+import { driverStatusMap } from "./mappers";
+
 export const driverTableConfig = [
   { Header: 'First Name', accessor: 'first_name' },
   { Header: 'Last Name', accessor: 'last_name' },
   { Header: 'Email', accessor: 'email' },
   { Header: 'Phone', accessor: 'phone' },
+  {
+    Header: "Status",
+    accessor: "is_active",
+    Cell: ({ row }) => {
+      return <span className="flex cursor-pointer items-center gap-2">
+        <span className={`${driverStatusMap[row.original.is_active].fill} w-2 h-2 rounded-sm`} />
+        {driverStatusMap[row.original.is_active].label}
+      </span>
+    }
+  }
 ];
---
+++ src/utils/mappers.js
@@ -0,0 +1,12 @@
+export const driverStatusMap = {
+  "true": {
+    id: 1,
+    label: "Active",
+    fill: "bg-green-500",
+  },
+  "false": {
+    id: 2,
+    label: "Inactive",
+    fill: "bg-red-500",
+  },
+};
