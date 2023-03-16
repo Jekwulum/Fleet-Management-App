1f754f617a87e5d2a1604c9aa817c756fe1401c6
Charles <charlesnwoye2@gmail.com>
2023-03-16 12:35:40 +0100

    DELETE trip

--- src/App.js
+++ src/App.js
@@ -1,7 +1,9 @@
 import { BrowserRouter as Router, Routes, Route, Navigate } from 'react-router-dom';
 import Driver from './pages/drivers/Driver';
 import Home from './pages/Home';
+import Maintenance from './pages/maintenance/Maintenance';
 import Vehicle from './pages/vehicles/Vehicle';
+
 import Trips from './pages/trips/Trips';
 import './App.css';
 
@@ -11,6 +13,7 @@ function App() {
       <Routes>
         {["/", "/home"].map((path, index) => <Route exact path={path} element={<Home />} key={index} />)}
         <Route exact path='/driver' element={<Driver />} />
+        <Route exact path='/maintenance' element={<Maintenance />} />
         <Route exact path='/vehicle' element={<Vehicle />} />
         <Route exact path='/trips' element={<Trips />} />
       </Routes>
